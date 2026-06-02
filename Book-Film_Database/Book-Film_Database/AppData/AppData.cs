f73449c29a70281640e8357ba495346a9bdb9266
Oliver!am <oliveriam1234@gmail.com>
2026-06-02 18:55:44 +0200

    Adding basic datatypes for Anime, Manga and AppData

--- Book-Film_Database/Book-Film_Database/AppData/AppData.cs
+++ Book-Film_Database/Book-Film_Database/AppData/AppData.cs
@@ -1,6 +1,20 @@
-﻿namespace Book_Film_Database.AppData;
+﻿using System.Collections.Generic;
+using Book_Film_Database.Models;
+
+namespace Book_Film_Database.AppData;
 
 public class AppData
 {
-    
+    public List<Anime> AnimeList { get; set; } = new List<Anime>();
+    public List<Manga> MangaList { get; set; } = new List<Manga>();
+
+    public void AddAnime(Anime NewAnime)
+    {
+        AnimeList.Add(NewAnime);
+    }
+
+    public void AddManga(Manga NewManga)
+    {
+        MangaList.Add(NewManga);
+    }
 }
--- Book-Film_Database/Book-Film_Database/Models/Product.cs
+++ Book-Film_Database/Book-Film_Database/Models/Product.cs
@@ -1,6 +1,30 @@
 ﻿namespace Book_Film_Database.Models;
 
-public class Product
+public abstract class Product
 {
-    
+
+}
+
+public class Anime : Product
+{
+    public int Ranks { get; set; }
+    public string Name { get; set; }
+    public string JapaneseName { get; set; }
+    public string Genre { get; set; }
+    public string Description { get; set; }
+    public string RelatedAnime { get; set; }
+    public string RelatedManga { get; set; }
+    public float Rating { get; set; }
+}
+
+public class Manga : Product
+{
+    public int Ranks { get; set; }
+    public string Name { get; set; }
+    public string JapaneseName  { get; set; }
+    public string Genre { get; set; }
+    public string Description { get; set; }
+    public string RelatedAnime { get; set; }
+    public string RelatedManga { get; set; }
+    public float Rating { get; set; }
 }
